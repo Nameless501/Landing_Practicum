91d9f25ee0505b62248ba43fbc2017a035e2c424
Eugene Zorin <ezorin5@yandex.ru>
2022-11-27 15:40:27 +0400

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,3 +10,58 @@ const validation = new FormValidator(
   formFeedback
 );
 validation.enableValidation();
+
+// // ------------- test type: scroll
+
+// const infoCardsListScroll = [...document.querySelectorAll('.scroll-card')].reverse();
+// const nextCardButtonScroll = document.querySelector('.test-button-2');
+
+// function switchCardScroll(cardList) {
+//   let index = 0;
+//   let buttonPosition = 0;
+
+//   // if(pageYOffset > 1550) {
+//   //   document.body.style.overflow = 'hidden';
+//   // }
+
+//   return function() {
+//     const currentCard = cardList[index];
+//     const nextCard = cardList[index + 1];
+
+//     if(index < cardList.length - 1) {
+//       currentCard.classList.remove('info__card_active');
+//       nextCard.classList.add('info__card_active');
+//       index = index + 1;
+//       buttonPosition += 203;
+
+//       if(index === cardList.length - 1) {
+//         nextCardButtonScroll.style.display = 'none';
+//       } else {
+//         nextCardButtonScroll.style.transform = `translate(${buttonPosition}px)`;
+//       }
+//     }
+//   }
+// }
+
+// const handleCardScroll = switchCardScroll(infoCardsListScroll);
+
+// let counter = 0;
+
+// function stopsScroll() {
+//   const last = infoCardsListScroll[infoCardsListScroll.length - 1];
+
+//   if(last.classList.contains('info__card_active')) {
+//     document.body.style.overflow = '';
+//     window.removeEventListener('mousewheel', stopsScroll);
+//   } else if(pageYOffset > 1550) {
+//     document.body.style.overflow = 'hidden';
+//     console.log(counter);
+//     counter = counter + 1;
+//     if(counter === 20) {
+//       handleCardScroll();
+//       counter = 0;
+//     }
+//   }
+// }
+
+// window.addEventListener('mousewheel', stopsScroll);
