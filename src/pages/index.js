79950d3539149ff5d707a1c607f79f4b1dbb6437
Sonyamaster1 <sonyamitrofanova3262@gmail.com>
2022-11-27 23:55:20 +0500

    Form, popup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import "./index.css";
 import FormValidator from "../components/FormValidator.js";
-import {formFeedback} from '../utils/constants.js';
+import { formFeedback, popupBtn, popup, closeBtn } from "../utils/constants.js";
 import { validationConfig } from "../utils/constants.js";
-
 // ------------- валидация формы
 
-const validation = new FormValidator(
-  validationConfig,
-  formFeedback
-);
+const validation = new FormValidator(validationConfig, formFeedback);
 validation.enableValidation();
+
+popupBtn.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
+});
+closeBtn.addEventListener("click", function () {
+  popup.classList.remove("popup_opened");
+});
